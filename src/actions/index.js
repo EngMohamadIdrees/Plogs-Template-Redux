5e8d04bb8a5418537ad5f1bbdd6acd57a9847a5c
Mohamad Idrees <106817730+EngMohamadIdrees@users.noreply.github.com>
2023-03-02 19:05:00 +0200

    First-Push

---
+++ src/actions/index.js
@@ -0,0 +1,47 @@
+import _ from "lodash";
+import jsonPlaceholder from "../apis/jsonPlaceholder";
+
+export const fetchPostsAndUsers = () => {
+  return async (dispatch, getState) => {
+    await dispatch(fetchPosts());
+
+    //Method 1
+    // const userIds = _.uniq(_.map(getState().posts, "userId"));
+    // userIds.forEach((id) => dispatch(fetchUser(id)));
+
+    // Methods 2
+    _.chain(getState().posts)
+      .map("userId")
+      .uniq()
+      .forEach((id) => dispatch(fetchUser(id)))
+      .value();
+  };
+};
+
+export const fetchPosts = () => {
+  return async (dispatch) => {
+    const response = await jsonPlaceholder.get("/posts");
+
+    dispatch({ type: "FETCH_REQUEST", payload: response.data });
+  };
+};
+
+export const fetchUser = (id) => {
+  return async (dispatch) => {
+    const response = await jsonPlaceholder.get(`/users/${id}`);
+    dispatch({ type: "FETCH_USER", payload: response.data });
+  };
+};
+
+// ********* Solution Using Memoize To make Just One Request Fro Each User *********
+
+// export const fetchUser = (id) => {
+//   return (dispatch) => {
+//     _fetchUser(id, dispatch);
+//   };
+// };
+
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//   const response = await jsonPlaceholder.get(`/users/${id}`);
+//   dispatch({ type: "FETCH_USER", payload: response.data });
+// });
---
+++ src/reducers/index.js
@@ -0,0 +1,7 @@
+import { combineReducers } from "redux";
+import postsReducers from "./postsReducers";
+import userReducers from "./userReducers";
+export default combineReducers({
+  posts: postsReducers,
+  users: userReducers,
+});
